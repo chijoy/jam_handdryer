a271340c15b285ae2cc43ca2685fc7ef5cb5925c
Joy <joymccaffreychicago@gmail.com>
2016-11-01 18:29:12 -0500

    Create dryer gem.

---
+++ lib/jam_handdryer.rb
@@ -0,0 +1,9 @@
+require "jam_handdryer/version"
+
+module JamHanddryer
+  class Dryer
+    def dry
+      `say ooooooooooo ooooooooo ooooooooooo ooooooooooooo`
+    end
+  end
+end
---
+++ lib/jam_handdryer/version.rb
@@ -0,0 +1,3 @@
+module JamHanddryer
+  VERSION = "0.1.0"
+end
